806a86e690ec4674537db80da0787cf03d65a341
Dave Price <d.m.price@soton.ac.uk>
2018-09-19 15:31:58 +0100

    rainfall_conversion.py

--- rainfall_conversion.py
+++ rainfall_conversion.py
@@ -13,3 +13,8 @@ def inches_to_mm(inches):
 
 #Todo: (DEV B): Add funtuon to convert from inches to cm
 
+    cm = inches * 2.55
+    return cm
+
+
+#TODO: (Jason): Can you finsh this code to complete my thesis is 3 days
